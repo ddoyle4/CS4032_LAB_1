2cf7a417ed895bdf0b1dc3cc3c47fb7500319bac
David Doyle <ddoyle3@tcd.ie>
2016-10-24 23:18:25 +0100

    Cleaning up, adding user instructions

--- Main.hs
+++ Main.hs
@@ -2,12 +2,18 @@ module Main where
 import Network.Socket hiding (sendTo, recv, recvFrom)
 import Network.Socket.ByteString (recv)
 import qualified Data.ByteString.Char8 as B8
+import Data.Char
 
 client' :: Int -> IO ()
 client' = client "localhost"
 
 main = do
-  client "localhost" 8002
+  putStrLn "Enter host:"
+  host <- getLine
+  putStrLn "Enter port:"
+  port <- getLine 
+  let portInt = read port :: Int
+  client host portInt
 
 client :: String -> Int -> IO ()
 client host port = withSocketsDo $ do
@@ -17,6 +23,7 @@ client host port = withSocketsDo $ do
                 connect sock (addrAddress serverAddr)
                 msgSender sock
                 sClose sock
+                client host port
 
 msgSender :: Socket -> IO ()
 msgSender sock = do
@@ -25,6 +32,6 @@ msgSender sock = do
   let msg = "GET /index.php?message=" ++ input ++ " HTTP/1.1\r\nHost: localhost:8002\r\n\r\n"
   putStrLn msg
   send sock msg
-  rMsg <- recv sock 2048
+  rMsg <- recv sock 4096
   B8.putStrLn rMsg
   --if msg == B8.pack "q" then putStrLn "Disconnected!" else msgSender sock
