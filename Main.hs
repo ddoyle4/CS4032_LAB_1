8f2c750b4d8d194d6d6ba847bb6d3764ec9870a0
David Doyle <ddoyle3@tcd.ie>
2016-11-02 19:50:52 +0000

    Added parsing of response. Added readme. Added host as specified by user to GET request header

--- Main.hs
+++ Main.hs
@@ -17,7 +17,7 @@ import Data.String.Utils
 -- Collects desire host and port
 main :: IO () 
 main = do
-  putStrLn "Enter host (e.g. localhost):"
+  putStrLn "Enter host:"
   host <- getLine
   putStrLn "Enter port:"
   port <- getLine 
@@ -29,37 +29,49 @@ client :: String -> Int -> IO ()
 client host port = withSocketsDo $ do
   addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
   let serverAddr = head addrInfo
-  session serverAddr
+  session host serverAddr
   putStrLn "finished"
 
 
 -- An interactive session which allows the user to send multiple GET requests to the server
-session :: AddrInfo -> IO ()
-session sa = do
+session :: String -> AddrInfo -> IO ()
+session host sa = do
   putStrLn "Enter text:"
   input <- getLine
   case input of
     "q" -> do 
       putStrLn "disconnected"
     _ -> do 
-    putStrLn input
-    getRequest sa (B8.pack (subStrRepl input " " "%20"))
-    session sa
+    getRequest host sa (B8.pack (subStrRepl input " " "%20"))
+    session host sa
 
 -- Creates a socket, sends the GET request, prints the response
-getRequest :: AddrInfo -> B8.ByteString -> IO ()
-getRequest addr input = do
+getRequest :: String -> AddrInfo -> B8.ByteString -> IO ()
+getRequest host addr input = do
   sock <- socket (addrFamily addr) Stream defaultProtocol
+  putStrLn host
   let startGetRequest = B8.pack "GET /index.php?message="
-  let endGetRequest = B8.pack " HTTP/1.1\r\n\r\n"
+  let endGetRequest = B8.pack (" HTTP/1.1\r\nHost: " ++ host ++ "\r\n\r\n")
   let getRequest = B8.append startGetRequest (B8.append input endGetRequest)
   connect sock (addrAddress addr)
   send sock getRequest
-  rMsg <- recv sock 8000
-  B8.putStrLn rMsg
+  rMsg <- recv sock 8192                                  --simple get requests won't require more than 8KB 
+  let response = B8.unpack $ rMsg
+  let h = Fields (head (split "\r\n\r\n" response)) (last (split "\r\n\r\n" response))  
+  putStrLn ("\n----------Header---------- \n\n" ++ (getHead h) ++ "\n--------------------------\n")
+  putStrLn ("\n-----------Body----------- \n\n" ++ (getBody h) ++ "\n--------------------------\n")
   sClose sock
 
 -- Helper method used to clean strings to be passed in GET request, i.e. can replace " " with "%20" 
 subStrRepl :: String -> String -> String -> String
 subStrRepl str find repl = join repl (split find str)
+
+-- Implementation of GET response data structure
+data GETResponse = Fields String String
+
+getHead :: GETResponse -> String
+getHead (Fields h b) = h
+
+getBody :: GETResponse -> String
+getBody (Fields h b) = b
 
---
+++ README.md
@@ -0,0 +1,13 @@
+Submission for Sockets 1 - To implement a simple echo client using raw sockets.
+
+Author: David Doyle
+
+#### Compiling
+Compile with `ghc -o client Main.hs`
+
+### Running
+Run with `./client`
+
+
+**Note**
+The built-in php server can sometimes give me trouble. I have tested the client against the same script, /server/index.php, when it's running on a proper apache server and it works perfectly.
